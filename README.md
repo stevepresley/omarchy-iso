4ad4aad4c060eb8f3f4a51f04e0898e6834e8626
Torxed <torxed@archlinux.org>
2025-07-27 16:42:10 +0200

    Added first version of ISO build

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # omarchy-iso
+
+This will create an ISO that does two things:
+
+1. Auto-boot into Live Arch Linux (without prompt)
+2. Will auto-launch `omarchy-tui`
+
+# Build instructions
+
+```
+$ docker run --rm \
+	--privileged \
+	-v "./out/:/out/" \
+	-v "./build_iso.sh:/build_iso.sh:ro" \
+	-v "./archiso:/archiso:ro" \
+	-v "./grub-autoboot.patch:/grub-autoboot.patch:ro" \
+	archlinux/archlinux:latest /build_iso.sh
+```
+
+Finished result should be in `./out`.
+Note that `--privileged` is used. The main reason being `mkarchiso` mounts `/sys/proc` and other locations, which requires privileged permissions.
+
+# Signing
+
+```
+$ gpg --local-user 0xD4B58E897A929F2E \
+  --output ./out/*.iso.sig \
+  --detach-sig ./out/*.iso
+```
+
+Note that `gpg --list-keys --fingerprint --keyid-format 0xLONG torxed@archlinux.org` can give you your GPG Key ID. In my case `ed25519/0xD4B58E897A929F2E`.
---
+++ build_iso.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+pacman-key --init
+pacman --noconfirm -Sy archlinux-keyring
+pacman --noconfirm -Sy archiso git
+
+# Packages to add to the archiso profile packages
+# As a base example (the TUI packages needs to be here)
+packages=(
+	git
+)
+
+cache_dir=$(realpath --canonicalize-missing ~/.cache/omarchy/iso_$(date +%Y-%m-%d))
+
+mkdir -p $cache_dir/
+cp -r archiso/configs/releng/* $cache_dir/
+
+# We add in our auto-start applications
+# We'll assume the TUI installer is called omarchy-tui for now
+cat <<- _EOF_ | tee $cache_dir/airootfs/root/.zprofile
+	omarchy-tui
+_EOF_
+
+# Add packages to the archiso profile packages
+for package in "${packages[@]}"; do
+	echo "$package" >> "$cache_dir/packages.x86_64"
+done
+
+# We patch grub to our liking:
+(cd $cache_dir/ && git apply /grub-autoboot.patch)
+# We could also use:
+# patch -p1 < grub-autoboot.patch
+
+mkarchiso -v -w "$cache_dir/work/" -o "/out/" "$cache_dir/"
