5994ea63e511be2458b4168c2cf0d328378d9941
David Heinemeier Hansson <david@hey.com>
2025-08-20 16:40:57 +0200

    Catch errors

--- builder/cmds/autostart.sh
+++ builder/cmds/autostart.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+catch_errors() {
+  echo -e "\n\e[31mOmarchy installation failed!\e[0m"
+  echo "The failing command was: \`$BASH_COMMAND\` (exit code: $?)"
+  echo "Get help from the community: https://discord.gg/tXFUdasqhY"
+
+  if [[ -n $OMARCHY_USER ]]; then
+    echo "You can retry by running: bash ~/.local/share/omarchy/install.sh"
+    HOME=/home/$OMARCHY_USER \
+      arch-chroot -u $OMARCHY_USER /mnt/ \
+      env OMARCHY_USER_NAME="$(<user_full_name.txt)" \
+      OMARCHY_USER_EMAIL="$(<user_email_address.txt)" \
+      /bin/bash
+  fi
+}
+
+trap catch_errors ERR
+
 if [[ $(tty) == "/dev/tty1" ]]; then
   NETWORK_NEEDED=1 ./installer
 
