57988b693d6419052b30595d10916bb39a9bf998
David Heinemeier Hansson <david@hey.com>
2025-08-25 14:25:52 +0200

    No we are shipping the real thing

--- builder/cmds/autostart.sh
+++ builder/cmds/autostart.sh
@@ -70,5 +70,5 @@ EOF
   chmod 440 /mnt/etc/sudoers.d/99-omarchy-installer
 
   # Run Omarchy web installer
-  chroot_bash -lc "curl -fsSL https://omarchy.org/install-dev | bash || bash"
+  chroot_bash -lc "curl -fsSL https://omarchy.org/install | bash || bash"
 fi
