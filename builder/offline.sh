c089a3b6f723cc510fd404ae2d262f56040c01dd
Ryan Hughes <ryan@heyoodle.com>
2025-08-30 18:35:48 +0300

    Tons of tweaks to reach working v1

--- builder/offline.sh
+++ builder/offline.sh
@@ -8,7 +8,12 @@ pacman-key --init
 pacman --noconfirm -Sy archlinux-keyring
 pacman --noconfirm -Sy archiso git python-pip sudo base-devel jq
 
-cache_dir=$(realpath --canonicalize-missing ~/.cache/omarchy/iso_$(date +%Y-%m-%d))
+# Use the date from the host if provided (to avoid timezone mismatches), otherwise use container date
+if [ -n "$ISO_BUILD_DATE" ]; then
+  cache_dir=$(realpath --canonicalize-missing ~/.cache/omarchy/iso_${ISO_BUILD_DATE})
+else
+  cache_dir=$(realpath --canonicalize-missing ~/.cache/omarchy/iso_$(date +%Y-%m-%d))
+fi
 offline_mirror_dir="$cache_dir/airootfs/var/cache/omarchy/mirror/offline"
 
 # We need to fiddle with pip settings
@@ -27,6 +32,7 @@ python_packages=(
 arch_packages=(
   git
   gum
+  jq
   openssl
   tzupdate # This is technically an AUR package
 )
@@ -38,7 +44,7 @@ prepare_offline_mirror() {
 
   # Combine all packages from both files into one array
   all_packages=()
-  for package_file in omarchy.packages archinstall.packages; do
+  for package_file in /builder/packages/omarchy.packages /builder/packages/archinstall.packages; do
     if [ -f "$package_file" ]; then
       echo "Reading $package_file..."
       while IFS= read -r package; do
@@ -62,6 +68,7 @@ prepare_offline_mirror() {
     sed -i 's/^#*DownloadUser = alpm/DownloadUser = root/' /etc/pacman.conf
 
     # Download all the packages to the offline mirror inside the ISO
+    echo "Downloading all packages (including AUR) to offline mirror: ${all_packages[@]}"
     pacman --config $cache_dir/pacman.conf \
       --noconfirm -Syw "${all_packages[@]}" \
       --cachedir $offline_mirror_dir/ \
@@ -81,14 +88,16 @@ make_archiso_offline() {
   # This function will simply disable any online activity we have.
   # for instance the reflector.service which tries to optimize
   # mirror order by fetching the latest mirror list by default.
-  #
-  # We'll leave some things online, like NTP as that won't
-  # interfere with anything, on the flip side it will help if we do
-  # have internet connectivity.
 
+  # Disable reflector
   rm "$cache_dir/airootfs/etc/systemd/system/multi-user.target.wants/reflector.service"
   rm -rf "$cache_dir/airootfs/etc/systemd/system/reflector.service.d"
   rm -rf "$cache_dir/airootfs/etc/xdg/reflector"
+
+  # Disable the WKD sync service that would try to fetch keys from the network
+  # but ensure the keyring is already populated
+  rm -f "$cache_dir/airootfs/etc/systemd/system/multi-user.target.wants/archlinux-keyring-wkd-sync.service"
+  rm -f "$cache_dir/airootfs/etc/systemd/system/timers.target.wants/archlinux-keyring-wkd-sync.timer"
 }
 
 mkdir -p $cache_dir/
@@ -97,90 +106,40 @@ mkdir -p $offline_mirror_dir/
 # We base our ISO on the official arch ISO (releng) config
 cp -r archiso/configs/releng/* $cache_dir/
 
+# Apply all the general patches early (same as online.sh)
+# Need to do this from the cache_dir
+cd $cache_dir
+for patch in /builder/patches/*.patch; do
+  # Skip profiledef.patch as it conflicts with offline/permissions.patch
+  if [[ "$(basename "$patch")" != "profiledef.patch" ]]; then
+    git apply "$patch"
+  fi
+done
+cd -
+
 prepare_offline_mirror
 make_archiso_offline
 
 # Insert the configurator in the root users home folder (default user in the official releng ISO profile).
-curl -fsSL -o "airootfs/root/configurator" \
+mkdir -p "$cache_dir/airootfs/root"
+curl -fsSL -o "$cache_dir/airootfs/root/configurator" \
   "https://raw.githubusercontent.com/$OMARCHY_CONFIGURATOR_REPO/$OMARCHY_CONFIGURATOR_REF/configurator"
 
 # Clone Omarchy itself
-git clone -b dev --single-branch https://github.com/basecamp/omarchy.git "$cache_dir/airootfs/root/omarchy"
+git clone -b $OMARCHY_INSTALLER_REF --single-branch https://github.com/$OMARCHY_INSTALLER_REPO.git "$cache_dir/airootfs/root/omarchy"
 
-# We add in our auto-start applications
-# First we'll check for an active internet connection
-# Then we'll start the omarchy installer
-cat <<-_EOF_ | tee $cache_dir/airootfs/root/.automated_script.sh
-	#!/usr/bin/env bash
+# Copy the autostart script (we'll need to create an offline version)
+cp /builder/cmds/autostart-offline.sh $cache_dir/airootfs/root/.automated_script.sh
 
-	if [[ \$(tty) == "/dev/tty1" ]]; then
-	    sh ./check_connectivity.sh && \
-	    sh ./installer && \
-	    archinstall \
-	    	--config user_configuration.json \
-	    	--creds user_credentials.json \
-	    	--silent && \
-	    export OMARCHY_USER=\`ls /mnt/home/\` && \
-
-	    mkdir -p /mnt/home/\$OMARCHY_USER/.local/share/ && \
-	    cp -r /root/omarchy "/mnt/home/\$OMARCHY_USER/.local/share/" && \
-	    chown -R 1000:1000 "/mnt/home/\$OMARCHY_USER/.local/" && \
-	    chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install.sh && \
-	    chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/apps/mimetypes.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/apps/webapps.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/apps/xtras.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/detect-keyboard-layout.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/fix-fkeys.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/network.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/nvidia.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/power.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/timezones.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/config.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/identification.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/increase-sudo-tries.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/config/login.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/asdcontrol.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/bluetooth.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/fonts.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/hyprlandia.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/printer.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/theme.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/desktop/desktop.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/development.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/nvim.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/ruby.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/docker.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/firewall.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/development/terminal.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/migrations.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/aur.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/guard.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/gum.sh && \
-		chmod +x /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/tte.sh && \
-		echo '' > /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/tte.sh && \
-		echo '' > /mnt/home/\$OMARCHY_USER/.local/share/omarchy/install/preflight/gum.sh && \
-	    
-	    # Copy sudoers config to target system for passwordless sudo in chroot
-	    mkdir -p /mnt/etc/sudoers.d && \
-	    cp /etc/sudoers.d/99-omarchy-installer /mnt/etc/sudoers.d/ && \
-	    echo "\$OMARCHY_USER ALL=(ALL:ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers.d/99-omarchy-installer && \
-	    
-	    HOME=/home/\$OMARCHY_USER arch-chroot -u \$OMARCHY_USER /mnt/ /bin/bash -c "source /home/\$OMARCHY_USER/.local/share/omarchy/install.sh"
-	fi
-_EOF_
-
-# We patch permissions, grub and efi loaders to our liking:
+# Apply offline-specific patches
 (cd $cache_dir/ && git apply /builder/patches/offline/permissions.patch)
-(cd $cache_dir/ && git apply /builder/patches/grub-autoboot.patch)
-(cd $cache_dir/ && git apply /builder/patches/efi-autoboot.patch)
-# We could also use:
-# patch -p1 < aur-mirror.patch
-# patch -p1 < permissions.patch
-# patch -p1 < grub-autoboot.patch
-# patch -p1 < efi-autoboot.patch
 
 # Remove the default motd
 rm "$cache_dir/airootfs/etc/motd"
+
+# Ensure pacman cache directory exists with proper permissions
+mkdir -p "$cache_dir/airootfs/var/cache/pacman/pkg"
+chmod 755 "$cache_dir/airootfs/var/cache/pacman/pkg"
 
 # Install Python packages for the installer into the ISO
 # file system.
@@ -210,8 +169,8 @@ iso_packages=($(cat "$cache_dir/packages.x86_64"))
 mkdir -p /tmp/cleandb
 
 echo "Populating host offline mirror with ISO packages: ${iso_packages[@]}"
-# And we have to use the hosts pacman.conf since all other pacman.conf
-# files are prepped for offline use by this point.
+# We need to exclude AUR packages like tzupdate since they're not in upstream repos
+# They should already be downloaded to the offline mirror from the earlier step
 pacman --config /etc/pacman.conf \
   --noconfirm -Syw $(echo "${iso_packages[@]}" | sed 's/tzupdate//g') \
   --cachedir "/var/cache/omarchy/mirror/offline/" \
