8d8b96b411f25abd112a4c0105edff44721f8ce2
Ryan Hughes <1630358+ryanrhughes@users.noreply.github.com>
2025-09-12 16:07:17 -0400

    Update includes

--- configs/airootfs/root/.automated_script.sh
+++ configs/airootfs/root/.automated_script.sh
@@ -124,9 +124,8 @@ if [[ $(tty) == "/dev/tty1" ]]; then
   fi
 
   source "$OMARCHY_INSTALL/preflight/set-size-vars.sh"
-  source "$OMARCHY_INSTALL/helpers/ansi-codes.sh"
   source "$OMARCHY_INSTALL/helpers/logo.sh"
-  source "$OMARCHY_INSTALL/preflight/set-gum-styling.sh"
+  source "$OMARCHY_INSTALL/preflight/gum.sh"
   source "$OMARCHY_INSTALL/helpers/tail-log-output.sh"
   source "$OMARCHY_INSTALL/helpers/trap-errors.sh"
 
