93b013a3cef350607784eea2a5fafb2c19277eb8
stevepresley <github@stevepresley.net>
2025-10-16 18:10:04 -0400

    Change ISO filename to include timestamp for multiple daily builds
    Previously: omarchy-2025.10.16-x86_64.iso
    Now: omarchy-20251016-180000-x86_64.iso
    
    This allows distinguishing between multiple builds on the same day
    by filename alone, without relying on filesystem timestamps.
    
    Format changed from %Y.%m.%d to %Y%m%d-%H%M%S

--- configs/profiledef.sh
+++ configs/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="omarchy"
 iso_label="OMARCHY_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Omarchy <https://omarchy.org>"
 iso_application="Omarchy Installer"
-iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m%d-%H%M%S)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux' 'uefi.grub')
